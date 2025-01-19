647960a16136e80231849b51cba753f8f81aae40
kibethh <kibethh@gmail.com>
2025-01-19 23:42:40 +0300

    Computed previous hash

---
+++ src/blockchain/mod.rs
@@ -0,0 +1,83 @@
+use sha2::{Digest, Sha256};
+use std::time::SystemTime;
+
+#[derive(Debug)]
+pub struct Block {
+    nonce: i32,
+    previous_hash: Vec<u8>,
+    time_stamp: u128,
+    transactions: Vec<Vec<u8>>,
+}
+
+impl Block {
+    pub fn new(nonce: i32, previous_hash: Vec<u8>) -> Self {
+        let time_now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        Self {
+            nonce,
+            previous_hash,
+            time_stamp: time_now.as_nanos(),
+            transactions: Vec::<Vec<u8>>::new(),
+        }
+    }
+
+    pub fn print(&self) {
+        // Formating value as hex
+        println!("Timestamp: {:x}", self.time_stamp);
+        // Formating value as integer
+        println!("Nonce: {}", self.nonce);
+        // Using Debug formatter for complex values
+        println!("Transactions: {:?}", self.transactions);
+        println!("previous_hash: {:?}", self.previous_hash);
+    }
+
+    pub fn hash(&self) -> Vec<u8> {
+        let mut bin = Vec::<u8>::new();
+        bin.extend(self.nonce.to_be_bytes());
+        bin.extend(self.previous_hash.clone());
+        bin.extend(self.time_stamp.to_be_bytes());
+        for tx in self.transactions.iter() {
+            bin.extend(tx);
+        }
+        let mut hasher = Sha256::new();
+        hasher.update(bin);
+        hasher.finalize().to_vec()
+    }
+}
+
+#[derive(Debug)]
+pub struct BlockChain {
+    transaction_pool: Vec<Vec<u8>>,
+    chain: Vec<Block>,
+}
+impl BlockChain {
+    pub fn new() -> Self {
+        let mut bc = BlockChain {
+            transaction_pool: Vec::<Vec<u8>>::new(),
+            chain: Vec::<Block>::new(),
+        };
+        bc.create_block(0, vec![0 as u8; 32]);
+        bc
+    }
+
+    pub fn create_block(&mut self, nonce: i32, previous_hash: Vec<u8>) {
+        let b = Block::new(nonce, previous_hash);
+        self.chain.push(b);
+    }
+
+    pub fn print(&self) {
+        for (i, block) in self.chain.iter().enumerate() {
+            println!("{} Chain: {} {}", "=".repeat(25), i, "=".repeat(25));
+            block.print();
+        }
+        println!("{}", "*".repeat(25));
+    }
+
+    pub fn last_block(&self) -> &Block {
+        if self.chain.len() > 1 {
+            return &self.chain[self.chain.len() - 1];
+        }
+        &self.chain[0]
+    }
+}
